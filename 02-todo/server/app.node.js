2f3165b429243deeebc7b0c16c24f9c137136a87
Lerky1337 <linneaerkas@hotmail.com>
2022-12-06 15:12:54 +0100

    kopierat

--- 02-todo/server/app.node.js
+++ 02-todo/server/app.node.js
@@ -1,7 +1,11 @@
-const http = require("http");
+const http = require('http');
 
 const server = http.createServer((req, res) => {
-console.log();
+  console.log(http.METHODS);
+
+  const statusCode = 425;
+  res.writeHead(statusCode);
+  res.end(`Du gjorde ett ${req.method}-anrop till ${req.url}`);
 });
 
-server.listen("5000", () => console.log("Server running on http://localhost:5000"));
+server.listen('5000', () => console.log('Server running on http://localhost:5000'));
